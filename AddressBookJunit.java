4469e1552d89fc9ea71b2d0626f80b866a05c9fd
Prasanna-Nanda <prasannanandakumar92@gmail.com>
2021-03-02 16:47:40 +0530

    Added UC2AddContact

--- AddressBookJunit.java
+++ AddressBookJunit.java
@@ -1,4 +1,13 @@
-private String firstName;
+import AddressServices.AddressService;
+
+import java.util.Scanner;
+
+public class AddressBookJunit {
+
+
+
+
+        private String firstName;
 	private String lastName;
 	private String address;
 	private String city;
@@ -89,7 +98,40 @@ private String firstName;
 
 	public static void main(String[] args) {
 
-		System.out.println("Welcome to address book program");
+		 int userInput;
+        int userChoice;
+        System.out.println("Welcome to address book program");
+        Scanner scanner = new Scanner(System.in);
+        AddressService addressService=new AddressService();
+
+        do {
+            System.out.println("1.Add record");
+            System.out.println("2.Edit record");
+            System.out.println("3.Delete record");
+            System.out.println("4.Display record");
+            userInput = scanner.nextInt();
+            switch(userInput)
+            {
+                case 1:
+                    addressService.add();
+                    break;
+                case 2:
+                    addressService.edit();
+                    break;
+                case 3:
+                    addressService.delete();
+                    break;
+                case 4:
+                    addressService.display();
+                    break;
+                default:System.out.println("Invalid option");
+            }
+
+            System.out.println("press 1 to start \n press 0 to disconnect");
+            userChoice=scanner.nextInt();
+        }while(userChoice==1);
+
+    }
 
 
 	}
---
+++ AddressServices/AddressService.java
@@ -0,0 +1,2 @@
+package AddressServices;public class AddressService {
+}
